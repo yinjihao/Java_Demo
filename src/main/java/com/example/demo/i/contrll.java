58f54bf94341cdfc9105e67138d85c38ce744f08
yinjihao <598941331@qq.com>
2019-07-12 19:36:52 +0800

    fix

--- src/main/java/com/example/demo/i/contrll.java
+++ src/main/java/com/example/demo/i/contrll.java
@@ -1,13 +1,17 @@
 package com.example.demo.i;
 
+import io.reactivex.Observable;
+import jdk.nashorn.internal.codegen.CompilerConstants;
+import okhttp3.OkHttpClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.Socket;
-import java.net.SocketAddress;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 public class contrll {
@@ -18,8 +22,32 @@ public class contrll {
 
     @RequestMapping(path = "/get")
     public String get() {
-        return "赵汉是狗";
+        return getData();
     }
+
+
+    private String getData() {
+        FutureTask<String> task = new FutureTask<String>(() -> call());
+        try {
+            return task.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return "1";
+    }
+
+    private String call() {
+        try {
+            TimeUnit.SECONDS.sleep(10);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "d";
+    }
+
+
 }
 
 
